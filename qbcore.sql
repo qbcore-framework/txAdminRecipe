2fa8461fcb0b4bce7e496a88d79baf4986a1a2f6
Kakarot <57848836+GhzGarage@users.noreply.github.com>
2021-07-26 01:20:33 -0500

    Update qbcore.sql

--- qbcore.sql
+++ qbcore.sql
@@ -321,7 +321,7 @@ CREATE TABLE `companies` (
 
 CREATE TABLE IF NOT EXISTS `gloveboxitems` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `plate` varchar(255) DEFAULT NULL,
+  `plate` varchar(255) NOT NULL '[]',
   `items` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
   PRIMARY KEY (`plate`),
   KEY `id` (`id`)
@@ -329,7 +329,7 @@ CREATE TABLE IF NOT EXISTS `gloveboxitems` (
 
 CREATE TABLE IF NOT EXISTS `stashitems` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `stash` varchar(255) DEFAULT NULL,
+  `stash` varchar(255) NOT NULL '[]',
   `items` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
   PRIMARY KEY (`stash`),
   KEY `id` (`id`)
@@ -337,7 +337,7 @@ CREATE TABLE IF NOT EXISTS `stashitems` (
 
 CREATE TABLE IF NOT EXISTS `trunkitems` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `plate` varchar(255) DEFAULT NULL,
+  `plate` varchar(255) NOT NULL '[]',
   `items` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
   PRIMARY KEY (`plate`),
   KEY `id` (`id`)
