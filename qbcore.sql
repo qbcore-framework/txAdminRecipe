56cde1e8f93abc1156b51614b4cc75b61a4808e8
Kakarot <57848836+GhzGarage@users.noreply.github.com>
2021-07-31 20:51:41 -0500

    Not Needed

--- qbcore.sql
+++ qbcore.sql
@@ -33,15 +33,6 @@ CREATE TABLE IF NOT EXISTS `bans` (
   KEY `ip` (`ip`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE IF NOT EXISTS `bills` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `citizenid` varchar(255) DEFAULT NULL,
-  `type` varchar(255) DEFAULT NULL,
-  `amount` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `citizenid` (`citizenid`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
-
 CREATE TABLE IF NOT EXISTS `crypto` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `crypto` varchar(50) NOT NULL DEFAULT 'qbit',
