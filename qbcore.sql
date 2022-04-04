43532136f7e1310edcf1b61c8c9b3a3d3e92f251
FjamZoo <34508112+FjamZoo@users.noreply.github.com>
2022-04-04 09:02:49 +0200

    Removed old SQL stuff

--- qbcore.sql
+++ qbcore.sql
@@ -216,18 +216,6 @@ CREATE TABLE IF NOT EXISTS `playerskins` (
   KEY `active` (`active`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1;
 
-CREATE TABLE IF NOT EXISTS `player_boats` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `citizenid` varchar(50) DEFAULT NULL,
-  `model` varchar(50) DEFAULT NULL,
-  `plate` varchar(50) DEFAULT NULL,
-  `boathouse` varchar(50) DEFAULT NULL,
-  `fuel` int(11) NOT NULL DEFAULT 100,
-  `state` int(11) NOT NULL DEFAULT 0,
-  PRIMARY KEY (`id`),
-  KEY `citizenid` (`citizenid`)
-) ENGINE=InnoDB AUTO_INCREMENT=1;
-
 CREATE TABLE IF NOT EXISTS `player_outfits` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `citizenid` varchar(50) DEFAULT NULL,
