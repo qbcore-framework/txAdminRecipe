d6a0d91c9957a2faa87fd7bd18870e1aa10fe619
Stevie <djstevie91@live.com>
2022-08-19 12:49:23 +0100

    QBcore SQL
    Just added the sql from the bank to the main

--- qbcore.sql
+++ qbcore.sql
@@ -271,13 +271,13 @@ CREATE TABLE IF NOT EXISTS `bank_accounts` (
   `businessid` int(11) DEFAULT NULL,
   `gangid` varchar(50) DEFAULT NULL,
   `amount` bigint(255) NOT NULL DEFAULT 0,
-  `account_type` enum('Current','Savings','Business','Gang') NOT NULL DEFAULT 'Current',
+  `account_type` enum('Current','Savings','business','Gang') NOT NULL DEFAULT 'Current',
   PRIMARY KEY (`record_id`),
   UNIQUE KEY `citizenid` (`citizenid`),
   KEY `business` (`business`),
   KEY `businessid` (`businessid`),
   KEY `gangid` (`gangid`)
-) ENGINE=InnoDB AUTO_INCREMENT=1;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 
 CREATE TABLE IF NOT EXISTS `bank_statements` (
   `record_id` bigint(255) NOT NULL AUTO_INCREMENT,
@@ -295,7 +295,19 @@ CREATE TABLE IF NOT EXISTS `bank_statements` (
   KEY `business` (`business`),
   KEY `businessid` (`businessid`),
   KEY `gangid` (`gangid`)
-) ENGINE=InnoDB AUTO_INCREMENT=1;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+
+CREATE TABLE IF NOT EXISTS `bank_cards` (
+  `record_id` bigint(255) NOT NULL AUTO_INCREMENT,
+  `citizenid` varchar(50),
+  `cardNumber` varchar(50) DEFAULT NULL,
+  `cardPin` varchar(50) DEFAULT NULL,
+  `cardActive` tinyint(4) DEFAULT 1,
+  `cardLocked` tinyint(4) DEFAULT 0,
+  `cardType` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`citizenid`),
+  KEY `record_id` (`record_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 
 CREATE TABLE IF NOT EXISTS `gloveboxitems` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
