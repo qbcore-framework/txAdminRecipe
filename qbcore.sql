7f2da6e62b658764f7b600d683ade32487c7624a
Brandon <98152656+BrandonSnippets@users.noreply.github.com>
2023-05-17 11:22:53 -0500

    Updated sql

--- qbcore.sql
+++ qbcore.sql
@@ -480,6 +480,18 @@ CREATE TABLE IF NOT EXISTS `mdt_impound` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+CREATE TABLE IF NOT EXISTS `mdt_clocking` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `user_id` varchar(50) NOT NULL DEFAULT '',
+  `firstname` varchar(255) NOT NULL DEFAULT '',
+  `lastname` varchar(255) NOT NULL DEFAULT '',
+  `clock_in_time` varchar(255) NOT NULL DEFAULT '',
+  `clock_out_time` varchar(50) DEFAULT NULL,
+  `total_time` int(10) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`user_id`) USING BTREE,
+  KEY `id` (`id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
 CREATE TABLE IF NOT EXISTS `scenes` (
   `id` int NOT NULL AUTO_INCREMENT,
   `creator` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
@@ -1386,3 +1398,53 @@ INSERT INTO `ak47_qb_cannabiscafev2_bong` (`id`, `stock`) VALUES
 CREATE TABLE IF NOT EXISTS `ak47_qb_cannabiscafev2_shop_items` (
   `stock` longtext COLLATE armscii8_bin DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=armscii8 COLLATE=armscii8_bin;
+
+DROP TABLE IF EXISTS `rcore_reports`;
+CREATE TABLE `rcore_reports` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `author` text NOT NULL,
+  `title` text NOT NULL,
+  `state` varchar(20) NOT NULL,
+  `tag` text NULL,
+  `created_at` datetime NOT NULL,
+  `in_resolve_at` datetime DEFAULT NULL,
+  `in_resolve_by` text DEFAULT NULL,
+  `resolved_at` datetime DEFAULT NULL,
+  `resolved_by` text DEFAULT NULL,
+  `players_in_area` text NOT NULL,
+  `chat_room` text NOT NULL,
+  `logger` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+DROP TABLE IF EXISTS `rcore_reports_tags`;
+CREATE TABLE `rcore_reports_tags` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `tag` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+DROP TABLE IF EXISTS `rcore_reports_admin_chat`;
+CREATE TABLE `rcore_reports_admin_chat` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `author` text NOT NULL,
+  `parentMessageId` int(11) DEFAULT NULL,
+  `message` text NOT NULL,
+  `created_at` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+DROP TABLE IF EXISTS `t1ger_gangs`;
+CREATE TABLE `t1ger_gangs` (
+	`id` INT(11) NOT NULL AUTO_INCREMENT,
+	`name` VARCHAR(100) NOT NULL,
+	`notoriety` INT(11) NOT NULL DEFAULT 0,
+	`cash` INT(11) NOT NULL DEFAULT 0,
+	`leader` VARCHAR(100) NOT NULL,
+	`ranks` LONGTEXT DEFAULT NULL,
+	`members` LONGTEXT DEFAULT NULL,
+	`markers` LONGTEXT DEFAULT NULL,
+	`disabled` TINYINT(1) NOT NULL DEFAULT 0,
+	`rackets` LONGTEXT DEFAULT NULL,
+	PRIMARY KEY (`id`)
+);
